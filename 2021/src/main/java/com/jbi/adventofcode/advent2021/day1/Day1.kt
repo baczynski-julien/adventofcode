f1b8444812b39e43aa7de106ceee7d4e069112ce
Julien Baczynski <julien.baczynsky@gmail.com>
2021-12-01 18:17:50 +0100

    Day1

--- 2021/src/main/java/com/jbi/adventofcode/advent2021/day1/Day1.kt
+++ 2021/src/main/java/com/jbi/adventofcode/advent2021/day1/Day1.kt
@@ -8,20 +8,54 @@ import java.io.BufferedReader
  */
 object Day1 : DailySolution2021() {
 
+    private lateinit var input: List<Int>
     override val runWithInput: Boolean
-        get() = false
-
+        get() = true
     override val expectedResultP1: Int
-        get() = 0
+        get() = 7
     override val expectedResultP2: Long
-        get() = 0
+        get() = 5
 
-    override fun part1(reader: BufferedReader): Any {
-        return 0
+    override fun prerunInput(reader: BufferedReader) {
+        input = buildInput(reader)
     }
+
+    override fun prerunSample(reader: BufferedReader) {
+        input = buildInput(reader)
+    }
+
+    override fun part1(reader: BufferedReader): Any =
+        getIncrements(input)
 
     override fun part2(reader: BufferedReader): Any {
-        return 0
+        val list = MutableList(input.size - 2) { 0 }
+        for (i in input.indices) {
+            if (i < list.size)
+                list[i] += input[i]
+            if (i - 1 >= 0 && i - 1 < list.size)
+                list[i - 1] += input[i]
+            if (i - 2 >= 0)
+                list[i - 2] += input[i]
+        }
+        return getIncrements(list)
     }
+
+    private fun getIncrements(list: List<Int>): Int {
+        var value = 0
+        for (i in 1 until list.size) {
+            if (list[i] > list[i - 1])
+                value++
+        }
+        return value
+    }
+
+    private fun buildInput(reader: BufferedReader): List<Int> =
+        mutableListOf<Int>().apply {
+            reader.useLines { sequence: Sequence<String> ->
+                sequence.forEach {
+                    add(Integer.parseInt(it))
+                }
+            }
+        }
 
 }
